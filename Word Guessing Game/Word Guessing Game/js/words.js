c5693098c897a767c6acaae4606ab80b25bba85f
Vijayshree05 <119340137+Vijayshree05@users.noreply.github.com>
2024-02-05 15:15:25 +0530

    Add files via upload

---
+++ Word Guessing Game/Word Guessing Game/js/words.js
@@ -0,0 +1,52 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+   
+   
+    {
+        word: "ebay",
+        hint: "online shopping site"
+    },
+    {
+        word: "golang",
+        hint: "programming language"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+   
+    {
+        word: "bugs",
+        hint: "related to programming"
+    },
+  
+    {
+        word: "gif",
+        hint: "a file format for image"
+    },
+   
+    {
+        word: "gpu",
+        hint: "computer component"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+   
+    {
+        word: "mysql",
+        hint: "a relational database system"
+    },
+  
+  
+   
+  
+]
